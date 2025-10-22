2a3423b4caa756a727c6c3f2f13e5838928463bd
Alexis Millereux <m.alexis1702@gmail.com>
2025-10-22 18:46:53 +0200

    Suppression du fichier

--- eau01.py
+++
@@ -1,9 +0,0 @@
-def threeDigitsCombination():
-	combinations = []
-	for firstDigit in range(0, 10):
-		for secondDigit in range(firstDigit + 1, 10):
-			for thirdDigit in range(secondDigit + 1, 10):
-				 combinations.append(f"{firstDigit}{secondDigit}{thirdDigit}")
-	return combinations
-
-print(threeDigitsCombination())
