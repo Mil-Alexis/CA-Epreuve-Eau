6895b07a0f0d32b301c6e0d5eea6b697e60fd452
Alexis M <m.alexis1702@gmail.com>
2025-10-28 18:53:41 +0100

    Ajout des fichiers

---
+++ eau05.py
@@ -0,0 +1,43 @@
+import sys
+import math
+
+def getArguments():
+    args = sys.argv[1:]
+    return args
+
+def isPrimeNumber(number: int):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def isValidArguments(args: list):
+    if len(args) != 1:
+        print("Un seul argument est attendu")
+        return False
+    elif not args[0].isdigit() or int(args[0]) < 0:
+        print(-1)
+        return False
+    else:
+        return True
+
+def getNexPrimeNumber(number: int):
+
+    number += 1
+
+    while not isPrimeNumber(number):
+        number += 1
+    return number
+
+
+def displayNextPrimeNumber():
+    if not isValidArguments(getArguments()):
+        return
+    number = int(getArguments()[0])
+    print(getNexPrimeNumber(number))
+
+displayNextPrimeNumber()
+
+
