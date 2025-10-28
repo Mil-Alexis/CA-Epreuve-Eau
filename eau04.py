3853cff57f4e0cf9b4ddf51dbe69defc83729c8d
Alexis Millereux <m.alexis1702@gmail.com>
2025-10-28 05:57:56 +0100

    Ajout des fichiers

--- eau04.py
+++ eau04.py
@@ -1,6 +1,7 @@
 import sys
 
-def getArgument():
+
+def getArguments():
     args = sys.argv[1:]
     return args
 
@@ -8,6 +9,29 @@ def isValidArguments(args: list):
     if len(args) != 1:
         print("Un seul argument est attendu")
         return False
-    if not args[0].isdigit() or int(args[0]) < 0:
+    elif not args[0].isdigit() or int(args[0]) < 0:
         print(-1)
-    
+        return False
+    else:
+        return True
+
+def getFibonacciNumberAtIndex(number: int):
+    if number == 0 or number == 1:
+        return number
+
+    n1 = 0
+    n2 = 1
+
+    for n in range(1, number):
+        n = n1 + n2
+        n1 = n2
+        n2 = n
+    return n
+
+def displayFibonacciNumberAtIndex():
+    if not isValidArguments(getArguments()):
+        return
+    number = int(getArguments()[0])
+    print(getFibonacciNumberAtIndex(number))
+
+displayFibonacciNumberAtIndex()
